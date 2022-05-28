e9ce2df01e8b383a806c37b752b93b84ed0155f0
Timothy Lassiter <timlassiter11@gmail.com>
2022-05-28 11:18:52 -0400

    Added ability to edit and delete users

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, EmailField, PasswordField, SubmitField, StringField
+from wtforms import BooleanField, EmailField, PasswordField, SubmitField, StringField, HiddenField
 from wtforms.validators import EqualTo, InputRequired
 
 
@@ -30,3 +30,21 @@ class ResetPasswordForm(FlaskForm):
     password2 = PasswordField('Confirm Password', validators=[
                               InputRequired(), EqualTo('password', message='Passwords do not match')])
     submit = SubmitField('Reset Password')
+
+
+class MethodForm(FlaskForm):
+    """Form with a hidden field containing the form method.
+    Used for handling method types not supported by standard 
+    browsers such as DELETE and PUT.
+    """
+    method = HiddenField(default='POST')
+
+    def validate(self, extra_validators=None):
+        if self.method.data == 'DELETE':
+            return self.csrf_token.validate(self)
+        return super().validate(extra_validators=extra_validators)
+
+class UserEditForm(MethodForm):
+    email = EmailField('Email', validators=[InputRequired()])
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
