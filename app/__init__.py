96751d06eb0d6dcf35815b0ddcbc0f87ae4cf157
Timothy Lassiter <timlassiter11@gmail.com>
2022-05-29 08:37:23 -0400

    Added database migration support

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from config import Config
 from flask import Flask
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+migrate = Migrate()
 login = LoginManager()
 mail = Mail()
 
@@ -15,8 +17,9 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     app.config['TRAP_HTTP_EXCEPTIONS'] = True
-    
+
     db.init_app(app)
+    migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
 
@@ -32,11 +35,6 @@ def create_app(config_class=Config):
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
 
-    if not os.path.exists('app.db'):
-        with app.app_context():
-            db.create_all()
-
     return app
-
 
 from app import models
